afdc98a4f0cbe31b8e68a4a4bcbcc733c84baa61
hailey <_____@___.com>
2024-05-02 18:38:32 -0500

    its a ray-lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,91 @@
 #include <raylib.h>
 
 /// @brief i_data[], o_data[], i_ld
-struct
+struct REG
 {
-public:
-    int x, y;
-    const char *name;
+    const int w = 100;
+    const int h = 32;
+    const int fontsize = 18;
+
+    float x, y;
+    const char *label;
     void draw()
     {
-        DrawRectangle(x, y, 100, 32, WHITE);
-        DrawText(name, x, y, 12, YELLOW);
+        DrawRectangle(x, y, w, h, WHITE);
+        DrawText(label, x + (w / 2) - (MeasureText(label, fontsize) / 2), y, fontsize, BLACK);
     }
-} PC;
+} PC, MAR, MDR;
+
+struct MUX
+{
+    const int triw = 25 / 2;
+    const int rectw = 100;
+    const int h = 32;
+    const int fontsize = 18;
+
+    float x, y;
+    const char *label;
+
+    void draw()
+    {
+
+        DrawRectangle(x, y, rectw, h, WHITE);
+        DrawText(label,
+                 (x + rectw / 2) - (MeasureText(label, fontsize) / 2), y,
+                 fontsize, BLACK);
+
+        DrawTriangle(
+            {x + triw, y},
+            {x, y},
+            {x, y + h},
+            BLACK);
+
+        DrawTriangle(
+            {(x + rectw) - triw, y},
+            {x + rectw, y + h},
+            {x + rectw, y},
+            BLACK);
+    }
+} MX_MAR;
+
+struct WIRE
+{
+};
 
 int main(int argc, char **argv)
 {
-    PC.name = "PC";
-    PC.x = 0;
-    PC.y = 0;
+    PC.label = "PC";
+    PC.x = 50;
+    PC.y = 50;
+
+    MAR.label = "MAR";
+    MAR.x = 50;
+    MAR.y = 100;
+
+    MDR.label = "MDR";
+    MDR.x = 50;
+    MDR.y = 150;
+
+    MX_MAR.label = "MAR";
+    MX_MAR.x = 50;
+    MX_MAR.y = 200;
 
     InitWindow(600, 400, "Little Cpu");
+
+    SetTargetFPS(30);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
         PC.draw();
-        DrawText("Hello world!", 50, 50, 12, WHITE);
+        MAR.draw();
+        MDR.draw();
+        MX_MAR.draw();
+
+        DrawFPS(0, 0);
         EndDrawing();
     }
-    
+
     CloseWindow();
 }
